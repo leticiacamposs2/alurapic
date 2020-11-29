2ba38410d0ba8a0c8e811a57793bfc5e844dfcb8
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-11-28 23:33:10 -0300

    arrumando import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { PhotosModule } from './photos/photos.module';
 import { AppComponent } from './app.component';
 import { ErrorsModule } from './errors/errors.module';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
