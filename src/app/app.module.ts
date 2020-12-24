7ba25e6009b81dafcc849db56847ee7ee3a0f4fc
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-24 17:04:37 -0300

    Integração com Font Awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { PhotosModule } from './photos/photos.module';
-import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
 import { AppRoutingModule } from './app.routing.module';
 
