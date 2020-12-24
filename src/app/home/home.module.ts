2eb15330132b876054297bc5b5251d4c01d1535c
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-24 18:27:30 -0300

    Validação de formulários

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
-    declarations: [ SignInComponent ]
+    declarations: [ SignInComponent ],
+    imports: [ CommonModule, ReactiveFormsModule ]
 })
 export class HomeModule { }
