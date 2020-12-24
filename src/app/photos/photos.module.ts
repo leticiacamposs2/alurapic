a235cf22c1bc9e027ee31e6ac12c5f67808adbb2
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-24 17:26:26 -0300

    Component container e ng-content

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -8,6 +8,7 @@ import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { CardModule } from '../shared/components/card/card.module';
 
 @NgModule({
     declarations: [ 
@@ -20,7 +21,8 @@ import { LoadButtonComponent } from './photo-list/load-button/load-button.compon
     ],
     imports: [ 
         HttpClientModule,
-        CommonModule 
+        CommonModule,
+        CardModule 
     ]
 })
 export class PhotosModule {}
