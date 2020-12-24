915d8b2be734a0436b3a0ac4c3d2739fef22fd4c
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-24 19:30:14 -0300

    O papel do BehaviorSubject

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 import * as jwt_decode from 'jwt-decode'
 
@@ -9,7 +9,7 @@ import { User } from './user';
 @Injectable({ providedIn: 'root' })
 export class UserService {
 
-    private userSubject = new Subject<User>();
+    private userSubject = new BehaviorSubject<User>(null);
 
     constructor(private tokenService: TokenService) {
         this.tokenService.hasToken() &&
