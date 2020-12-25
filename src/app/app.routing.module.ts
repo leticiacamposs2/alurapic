2e995c552bb82b4a01d76d50a05ac6d6f1891671
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-25 10:53:10 -0300

    Implementando o Lazy loading

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,54 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from './core/auth/auth.guard';
-
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 
 import { NotFoundComponent } from './errors/not-found/not-found.component'; 
 
-import { SignInComponent } from './home/signin/signin.component';
-import { SignUpComponent } from './home/signup/signup.component';
-import { HomeComponent } from './home/home.component';
-
 const routes: Routes = [
     {
         path: '',
-        component: HomeComponent,
-        canActivate: [AuthGuard],
-        children: [
-            {
-                path: '',
-                component: SignInComponent,
-            },
-            {
-                path: 'signup',
-                component: SignUpComponent,
-            },
-        ]
+        pathMatch: 'full',
+        redirectTo: 'home'
     },
     {
-        path: '',
-        component: SignInComponent,
-        canActivate: [AuthGuard]
+        path: 'home',
+        loadChildren: './home/home.module#HomeModule'
     },
-    { 
-        path: 'user/:userName', 
+    {
+        path: 'user/:userName',
         component: PhotoListComponent,
         resolve: {
             photos: PhotoListResolver
         }
     },
-    { 
-        path: 'p/add', 
-        component: PhotoFormComponent 
+    {
+        path: 'p/add',
+        component: PhotoFormComponent
     },
-    { 
-        path: '**', 
-        component: NotFoundComponent 
-    }  
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
 ];
 
 @NgModule({
@@ -58,4 +41,3 @@ const routes: Routes = [
     exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
-
