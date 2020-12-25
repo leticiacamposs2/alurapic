d1f11f262fa86685b347d118b960ae454cac8024
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-25 14:19:33 -0300

    Seleção automática de arquivos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form.component';
 import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
 import { PhotoModule } from '../photo/photo.module';
+import { ImmediateClickModule } from 'src/app/shared/directives/immediate-click/immediate-click.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
@@ -15,7 +16,8 @@ import { PhotoModule } from '../photo/photo.module';
         VMessageModule,
         FormsModule,
         RouterModule,
-        PhotoModule
+        PhotoModule,
+        ImmediateClickModule
      ]
 })
 export class PhotoFormModule { }
