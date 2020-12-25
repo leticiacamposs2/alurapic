99b74000ea62b21c0d85c07dbaa8d159be1fce07
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-25 20:57:27 -0300

    resolvendo bug

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { PhotoDetailsComponent } from "./photo-details.component";
+import { CommonModule } from "@angular/common";
+import { PhotoModule } from "../photo/photo.module";
 
-import { PhotoDetailsComponent } from './photo-details.component';
-import { PhotoModule } from '../photo/photo.module';
-
-NgModule({
+@NgModule({
     declarations: [PhotoDetailsComponent],
+    exports: [PhotoDetailsComponent],
     imports: [
         CommonModule,
         PhotoModule
-    ],
-    exports: [PhotoDetailsComponent]
+    ]
 })
 export class PhotoDetailsModule { }
