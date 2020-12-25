72209df2a1a9ed08859efea2816a57e407d53fe3
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-25 20:45:34 -0300

    Buscando a foto selecionada da API

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PhotoDetailsComponent } from './photo-details.component';
+import { PhotoModule } from '../photo/photo.module';
 
 NgModule({
     declarations: [PhotoDetailsComponent],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        PhotoModule
+    ],
     exports: [PhotoDetailsComponent]
 })
 export class PhotoDetailsModule { }
