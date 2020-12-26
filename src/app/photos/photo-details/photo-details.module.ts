444ae6c23b066e0ca68afa5481c48fad6f09ead4
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-25 22:08:27 -0300

    Ocultando elementos que requerem permissão

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 
-import { PhotoDetailsComponent } from "./photo-details.component";
+import { PhotoDetailsComponent } from './photo-details.component';
 import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
-import { PhotoModule } from "../photo/photo.module";
+import { PhotoModule } from '../photo/photo.module';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
+import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.directive';
 
 @NgModule({
     declarations: [
         PhotoDetailsComponent,
-        PhotoCommentsComponent
+        PhotoCommentsComponent,
+        PhotoOwnerOnlyDirective
     ],
     exports: [
         PhotoDetailsComponent,
---
+++ src/app/photos/photo-details/photo-owner-only/photo-owner-only.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Input } from '@angular/core';
+
+import { UserService } from 'src/app/core/user/user.service';
+import { Photo } from 'src/app/photos/photo/photo';
+
+@Directive({
+    selector :'[photoOwnerOnly]'
+})
+export class PhotoOwnerOnlyDirective {
+
+    @Input() ownedPhoto: Photo;
+
+    constructor(
+        private element: ElementRef<any>,
+        private renderer: Renderer,
+        private userService: UserService) { }
+
+    ngOnInit() : void {
+        this.userService
+            .getUser()
+            .subscribe(user => {
+                 if(!user || user.id != this.ownedPhoto.userId) {
+                    this.renderer.setElementStyle(
+                        this.element.nativeElement, 'display', 'none'
+                    );
+                }
+            });
+    }
+}
