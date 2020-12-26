1620a7ee6b91327f74b9f89c570f13c2a8906c87
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-12-25 21:34:34 -0300

    Lidando com o refresh de comentários

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 import { PhotoService } from '../../photo/photo.service';
 import { PhotoComments } from './photo-comments';
@@ -29,12 +30,13 @@ export class PhotoCommentsComponent implements OnInit {
 
     save() {
         const comment = this.commentForm.get('comment').value as string;
-        this.photoService.addComment(this.photoId, comment)
-            .subscribe(() => {
-                this.commentForm.reset();
-                alert('Comentário adicionado com sucesso');
-    
-            })
+        this.comments$ = this.photoService
+                .addComment(this.photoId, comment)
+                .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+                .pipe(tap(() => {
+                    this.commentForm.reset();
+                    alert('Comentário adicionado com sucesso');
+          }));
     }
 
 }
